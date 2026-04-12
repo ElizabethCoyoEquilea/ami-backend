b45e5cc35851a063c5a34120d48557f07efdbb41
Ayrton Daza Miranda <ayrton.daza.1230@gmail.com>
2026-04-12 16:13:44 -0400

    Permitir front

--- app/main.py
+++ app/main.py
@@ -1,10 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import engine, Base
 from app.routers.usuarios_router import router as usuarios_router
 from app.routers.auth_router import router as auth_router
 import app.models
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:4200",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 Base.metadata.create_all(bind=engine)
 
